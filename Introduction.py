ecd7edf096fac72a31080328feeb8ee40c6c4ccd
Shreelu Santosh <94289402+ShreeluSantosh@users.noreply.github.com>
2024-05-17 18:39:08 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -29,5 +29,5 @@ with tab2:
 
    st.subheader("More features will be added in near future!")
 with tab3:
-   from pages._3_References import References
+   from pages.References import References
    References.example()
