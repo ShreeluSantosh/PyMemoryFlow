2a512df7163b81b5bfa32cc25aaaa2051faae5a8
Shreelu Santosh <94289402+ShreeluSantosh@users.noreply.github.com>
2024-05-17 08:59:43 +0530

    Added test Python samples

---
+++ test1.py
@@ -0,0 +1,6 @@
+def main():
+    for i in range(100):
+        a = [1]*(10 ** 6)
+        b = [2]*(10 ** 6)
+
+main()
---
+++ test2.py
@@ -0,0 +1,6 @@
+def main():
+    a = 100
+    for i in range(100000):
+        a *= i
+
+main()
